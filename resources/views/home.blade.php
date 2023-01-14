ebdfa37109061522df81592a90dc11c6f605a639
syafiquel <syafiqul.syazwan@gmail.com>
2023-01-14 17:40:18 +0800

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,8 @@
         <title></title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="css/respo.css">
+        <link rel="stylesheet" href="app.css">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
         <link href="http://fonts.cdnfonts.com/css/nexa-bold" rel="stylesheet">
     </head>
     <body class="flex">
@@ -117,15 +118,34 @@
     </ul>
 </nav>
 <section class="section__main">
-    <div class="flex search mb-8">
-        <button class="bg-white search__button"><svg width="24" height="24" viewBox="0 0 24 24" fill="none"
-                xmlns="http://www.w3.org/2000/svg">
-                <path fill-rule="evenodd" clip-rule="evenodd"
-                    d="M11.7388 2C17.1088 2 21.4768 6.368 21.4768 11.738C21.4768 14.2715 20.5045 16.5823 18.9134 18.3165L22.0442 21.4407C22.3372 21.7337 22.3382 22.2077 22.0452 22.5007C21.8992 22.6487 21.7062 22.7217 21.5142 22.7217C21.3232 22.7217 21.1312 22.6487 20.9842 22.5027L17.8156 19.343C16.1488 20.6778 14.0354 21.477 11.7388 21.477C6.36876 21.477 1.99976 17.108 1.99976 11.738C1.99976 6.368 6.36876 2 11.7388 2ZM11.7388 3.5C7.19576 3.5 3.49976 7.195 3.49976 11.738C3.49976 16.281 7.19576 19.977 11.7388 19.977C16.2808 19.977 19.9768 16.281 19.9768 11.738C19.9768 7.195 16.2808 3.5 11.7388 3.5Z"
-                    fill="#919EAB" />
-            </svg>
-        </button>
-        <input type="text" placeholder="Search anything" class="search__input">
+    <div class="flex flex-align-center flex-justify-between">
+        <div class="flex search mb-8">
+            <button class="bg-white search__button"><svg width="24" height="24" viewBox="0 0 24 24" fill="none"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" clip-rule="evenodd"
+                        d="M11.7388 2C17.1088 2 21.4768 6.368 21.4768 11.738C21.4768 14.2715 20.5045 16.5823 18.9134 18.3165L22.0442 21.4407C22.3372 21.7337 22.3382 22.2077 22.0452 22.5007C21.8992 22.6487 21.7062 22.7217 21.5142 22.7217C21.3232 22.7217 21.1312 22.6487 20.9842 22.5027L17.8156 19.343C16.1488 20.6778 14.0354 21.477 11.7388 21.477C6.36876 21.477 1.99976 17.108 1.99976 11.738C1.99976 6.368 6.36876 2 11.7388 2ZM11.7388 3.5C7.19576 3.5 3.49976 7.195 3.49976 11.738C3.49976 16.281 7.19576 19.977 11.7388 19.977C16.2808 19.977 19.9768 16.281 19.9768 11.738C19.9768 7.195 16.2808 3.5 11.7388 3.5Z"
+                        fill="#919EAB" />
+                </svg>
+            </button>
+            <input type="text" placeholder="Search anything" class="search__input">
+        </div>
+        <div class="flex flex-align-center flex-justify-between">
+            <ul class="profile-wrapper">
+                <li class="flex flex-justify-between flex-align-center mb-8">
+                    <img class="profile__pic mr-2"
+                        src="https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+                        alt="Timothy" width="32" height="32">
+                    <p class="text-bold mr-2">Timothy</p>
+                    <svg width="20" height="20" viewBox="5 5 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"></svg>
+                    <a href="#" class="name"></a>
+                    <ul class="menu">
+                        <li><a href="#">Edit</a></li>
+                        <li><a href="#">Change Password</a></li>
+                        <li><a href="#">Log out</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
     </div>
     <div class="bg-primary flex flex-justify-between flex-align-center banner">
         <div>
@@ -290,7 +310,25 @@
         <canvas id="myChart"></canvas>
     </div>
 </section>
-<section class="section__account">
+<nav class="bottom_nav">
+  <a href="#" class="mobile_nav_link">
+    <i class="material-icons mobile_nav_icon">dashboard</i>
+    <span class="mobile_nav_text">Dashboard</span>
+  </a>
+  <a href="#" class="mobile_nav_link mobile_nav_link--active">
+    <i class="material-icons mobile_nav_icon">person</i>
+    <span class="mobile_nav_text">Profile</span>
+  </a>
+  <a href="#" class="mobile_nav_link">
+    <i class="material-icons mobile_nav_icon">devices</i>
+    <span class="mobile_nav_text">Devices</span>
+  </a>
+  <a href="#" class="mobile_nav_link">
+    <i class="material-icons mobile_nav_icon">settings</i>
+    <span class="mobile_nav_text">Settings</span>
+  </a>
+</nav>
+<!-- <section class="section__account">
     <div class="flex flex-align-center flex-justify-between mb-8 option">
         <div class="notif">
             <svg width="32" height="32" viewBox="0 0 18 20" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -447,8 +485,8 @@
             <p class="text-bold text-success">+1.05 ETH</p>
         </div>
     </div>
-</section>
+</section> -->
         <script src="https://cdn.jsdelivr.net/npm/chart.js@3.7.1/dist/chart.min.js"></script>
-        <script src="js/respo.js"></script>
+        <script src="app.js"></script>
     </body>
 </html>
