fad37ab2fb69b98ec016848d6ac0b37377feb5a5
syafiquel <syafiqul.syazwan@gmail.com>
2023-01-15 13:45:29 +0800

    auth

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::group(['middleware' => ['auth']], function() {
+Route::middleware('auth')->group(function() {
 
     Route::get('/', function () {
         return view('home');
