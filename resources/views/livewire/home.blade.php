40bef253a1ff1a90f4a78de34e023b80454513cb
syafiquel <syafiqul.syazwan@gmail.com>
2023-03-03 12:29:04 +0800

    fix

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -19,7 +19,7 @@
                         </div>
                     </div>
                     <button x-data="{ price: {{ $nft_asset->price }}, tokenId: {{ $nft_asset->token_id }} }" x-init="" x-on:click="const web3 = new Web3(window.ethereum);
-                        await web3.eth.getBalance(window.ethereum.selectedAddress)
+                        web3.eth.getBalance(window.ethereum.selectedAddress)
                         .then(console.log);
                         console.log(price);
                         if(10000 > price) {
